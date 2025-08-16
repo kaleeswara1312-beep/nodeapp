a85d1e603e978c075f3bce4852fe7d4c64ead639
kaleeswarakumarm <kaleeswarakumarm@prevaj.com>
2025-08-16 13:44:55 +0530

    Simple HTTP server

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-console.log('testing')
+const http = require("http");
 
-console.log('final test')
+// Create a server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200; // success
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, World!\n");
+});
+
+// Server listens on port 3000
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000/");
+});
