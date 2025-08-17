bd57711ef6d80d520e2f5b89b700b0b1008e7841
kaleeswarakumarm <kaleeswarakumarm@prevaj.com>
2025-08-17 09:23:32 +0530

    Updated the response

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   res.statusCode = 200; // success
   res.setHeader("Content-Type", "text/plain");
-  res.end("New changes!\n");
+  res.end("Hello World!\n");
 });
 
 // Server listens on port 3000
